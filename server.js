a740a50ca544dade53225244fbbb8cd578ce8e37
vedulagayatri <vedulagayatri45@gmail.com>
2016-11-09 22:56:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/images.png', function (req, res) {
+app.get('/imad-2016-app/ui/images.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'images.png'));
 });
 
